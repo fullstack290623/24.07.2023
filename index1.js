5b262cfd38e28eeb44a7b336297aa19a6e4fb144
micserv010821 <fullstack290623@gmail.com>
2023-07-24 18:49:37 +0300

    Update index1.js

--- index1.js
+++ index1.js
@@ -7,15 +7,22 @@ sum = a + b; // 4 + 5 = 9
 diff = a - b;
 mul = a * b;
 the_div = a / b;
-// power2 of a 
-// power2 of b
-// avg of a and b
-// *bonus: sqrt a
-// *bonus: sqrt b
+var power2_a = a * a
+var power2_b = b * b
+var avg_a_b = (a + b) / 2
+var sqrt_a = Math.sqrt( a )
+var sqrt_b = Math.sqrt( b )
 
 document.write("<h3>Hello JS! </h3>");
 document.write("a + b = ", sum)
 document.write("<br /> a - b = ", diff)
 document.write("<br /> a * b = ", mul)
 document.write("<br /> a / b = ", the_div)
-// add the results here
+document.write("<br /> a * a = ", power2_a)
+document.write("<br /> b * b = ", power2_b)
+document.write("<br /> avg (a, b) = ", avg_a_b)
+document.write("<br /> a * a = ", power2_b)
+document.write("<br /> a sqrt = ", sqrt_a)
+document.write("<br /> b sqrt = ", sqrt_b)
+
+
