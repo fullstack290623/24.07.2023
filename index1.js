afbb2d8081935270ddac89a8efbe6b8eebcfbd71
micserv010821 <dotnet290320@gmail.com>
2023-07-24 18:27:15 +0300

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,2 +1,15 @@
 
+var sum, diff, mul, the_div
+var a = 4
+var b = 5
+
+sum = a + b; // 4 + 5 = 9
+diff = a - b;
+mul = a * b;
+the_div = a / b;
+
 document.write("<h3>Hello JS! </h3>");
+document.write("a + b = ", sum)
+document.write("<br />a - b = ", diff)
+document.write("<br />a * b = ", mul)
+document.write("<br />a / b = ", the_div)
